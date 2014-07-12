4623c9f140d3665729d68afd62933b63a3ccd5bd
Alyssa <alyssaquek@gmail.com>
2014-07-12 14:57:56 +0800

    Files to find tech meetup events in singapore

---
+++ README.md
@@ -0,0 +1,66 @@
+# Find Meetup events in your country
+
+Current sample finds tech events (category 34) in Singapore and filters out
+certain groups and works listed in the blacklist.
+
+## Usage
+1. Install dependencies `npm install`
+1. Open `config.js` and add your [meetup API key](https://secure.meetup.com/meetup_api/key/)
+1. Run `node index.js`. Events will be saved in `events.json` unless configured.
+
+## Configurations
+Sample in `config.js`
+The only required configuration is your `meetup API key`.
+The remaining configs can be safely deleted if not required.
+
+#### meetupParams - object
+Query parameters to find groups.
+Reponse returns public groups and the next event if available.
+URL to the event can be constructed by the event name and the event id.
+More filters can be found at: <http://www.meetup.com/meetup_api/docs/find/groups/>
+
+    meetupParams: {
+      key: process.env.MEETUP_API_KEY ||
+        '<your_api_key>',
+      country: 'SG',
+      category: 34,
+      page: 500
+    }
+
+#### blacklistGroups - array
+Array of group ids to be blacklisted.
+Any events from these groups will not be included.
+
+    blacklistGroups: [9319232]
+
+#### blacklistWords - array
+Array of words in a group name to be blacklisted.
+Group names containing any of these words will not be included.
+
+    blacklistNameWords: ['business', 'networking']
+
+#### outfile - string
+Optional outfile to save the JSON list of events
+
+    outfile: 'events.json'
+
+## Sample output
+
+    [
+      {
+        "time": "2014-07-16T11:15:00.000Z",
+        "id": "193467102",
+        "name": "DataScience SG Meetup - HR Analytics",
+        "groupName": "DataScience SG",
+        "groupUrl": "http://www.meetup.com/DataScience-SG-Singapore/",
+        "url": "http://meetup.com/DataScience-SG-Singapore/events/193467102"
+      },
+      {
+        "time": "2014-07-14T10:45:00.000Z",
+        "id": "193625622",
+        "name": "Technical workshop - Revolution Analytics and Cloudera",
+        "groupName": "R User Group - Singapore (RUGS)",
+        "groupUrl": "http://www.meetup.com/R-User-Group-SG/",
+        "url": "http://meetup.com/R-User-Group-SG/events/193625622"
+      }
+    ]
---
+++ index.js
@@ -0,0 +1,71 @@
+var querystring = require('querystring');
+var https = require('https');
+var Promise = require('promise');
+var jf = require('jsonfile');
+var config = require('./config');
+
+var meetupQuery = querystring.stringify(config.meetupParams);
+
+function https_get_json(url) {
+ return new Promise(function (resolve, reject) {
+    https.get(url, function (res) {
+      var buffer = [];
+      res.on('data', Array.prototype.push.bind(buffer));
+      res.on('end', function () {
+        var text = buffer.join('');
+        var json = JSON.parse(text);
+        if (res.statusCode < 400) {
+          resolve(json);
+        } else {
+          console.error('Err! HTTP status code:', res.statusCode, url);
+          reject(Error(text));
+        }
+      });
+    }).on('error', function (err) {
+      console.error('Err! HTTP request failed:', err.message, url);
+      reject(err);
+    });
+  });
+}
+
+function isValidGroup(row) {
+  var blacklistGroups = config.blacklistGroups || [];
+  var blacklistWords = config.blacklistWords || [];
+  var blacklistRE = new RegExp(blacklistWords.join('|'), 'i');
+
+  return blacklistWords.length === 0 ? true : !row.name.match(blacklistRE) &&
+         !blacklistGroups.some(function(id) { return row.id === id })
+         && row.country === 'SG'; //meetup adds JB events into SG :S
+}
+
+function saveEvents(arr, row) {
+  if (!(row.next_event && row.next_event.time)) return
+
+  var entry = row.next_event;
+  entry.group_name = row.name;
+  entry.group_url = row.link;
+  entry.url = 'http://meetup.com/' + row.urlname + '/events/' + entry.id;
+  entry.time = new Date(entry.time).toISOString();
+  events.push(entry);
+  console.log(entry.group_name + ' -- ' + entry.name + ' - ' + entry.url);
+}
+
+function saveToJson(data) {
+  if (!config.outfile) return
+  jf.writeFile(config.outfile, data, function(err) {
+    if (err) console.error(err);
+  })
+  console.log('JSON file saved at: ' + config.outfile)
+}
+
+https_get_json('https://www.meetup.com/muapi/find/groups?' + meetupQuery)
+  .then(function(data) {
+    events = [];
+    data
+      .filter(isValidGroup)
+      .reduce(saveEvents, events);
+    saveToJson(events);
+  })
+  .catch(function(err) {
+    console.error(err);
+  })
