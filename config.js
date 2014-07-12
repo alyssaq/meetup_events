5ac52fda89794643fea98f981c94de54b0e27fe3
Alyssa <alyssaquek@gmail.com>
2014-07-12 19:46:33 +0800

    Added next_event true to params to only show groups that have an upcoming event

--- config.js
+++ config.js
@@ -4,7 +4,8 @@ module.exports = {
       '496472523e5d5f6a6a3b6815126bf40',
     country: 'SG',
     category: 34,
-    page: 500
+    page: 500,
+    next_event: true
   },
 
   blacklistGroups: [9319232],
