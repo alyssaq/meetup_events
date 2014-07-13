721138397e448593f187e3b75cdb95c74b61ac0f
Alyssa <alyssaquek@gmail.com>
2014-07-13 17:49:57 +0800

    Fix utc timestamp

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function saveEvents(arr, row) {
   entry.group_name = row.name;
   entry.group_url = row.link;
   entry.url = 'http://meetup.com/' + row.urlname + '/events/' + entry.id;
-  entry.formatted_time = moment(new Date(entry.time)).format('DD MMM, ddd, h:mm a');
+  entry.formatted_time = moment.utc(entry.time + entry.utc_offset).format('DD MMM, ddd, h:mm a');
   events.push(entry);
   //console.log(entry.group_name + ' -- ' + entry.name + ' - ' + entry.url);
 }
